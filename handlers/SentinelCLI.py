5f6f1fd5e674439b88e97aee02ddbf10cb9f17a8
Alessandro Maggio <alex.tkd.alex@gmail.com>
2023-08-30 15:33:32 +0200

    Handle class for SentineCLI only manage keyring

---
+++ handlers/SentinelCLI.py
@@ -0,0 +1,47 @@
+import platform
+import os
+import stat
+import urllib.request
+from subprocess import run, PIPE
+
+class SentinelCLI():
+    def __init__(self):
+        # Linux: Linux
+        # Mac: Darwin
+        # Windows: Windows
+        platform_name = platform.system()
+        base_url = "https://github.com/freQniK/cli-client/releases/download/v0.3.1"
+        fnames = {
+            "Linux": "sentinelcli_linux_x86_64",
+            "Darwin": "sentinelcli_darwin_arm64",
+            "Windows": "sentinelcli.exe",
+        }
+        client_path = os.path.join(os.getcwd(), fnames[platform_name])
+        if not os.path.exists(client_path):
+            full_url = os.path.join(base_url, fnames[platform_name])
+            urllib.request.urlretrieve(full_url, client_path)
+
+            if platform_name != "Windows":
+                st = os.stat(client_path)
+                os.chmod(client_path, st.st_mode | stat.S_IEXEC)
+
+        keyring_path = os.path.join(os.getcwd(), "keyring")
+        self.based_cmd = f'{client_path} keys add --home "{keyring_path}" '
+
+    def create_key(self, key_name: str, backend: str = "test", password: str = None) -> str:
+        cmd = self.based_cmd + f"--keyring-backend {backend} {key_name}"
+        if backend == "test":
+            p = run(cmd, shell=True, encoding='ascii', stdout=PIPE, stderr=PIPE)
+            return f"{p.stdout} {p.stderr}"
+        elif backend == "file":
+            if password is None:
+                return "Please provide a pasword to use for backend file"
+            p = run(cmd, shell=True, encoding='ascii', stdout=PIPE, stderr=PIPE, input=f"{password}\n" * 2)
+            return f"{p.stdout} {p.stderr}"
+
+    def recovery_key(self, key_name: str, mnemonic: str, backend: str = "test", password: str = None) -> str:
+        if password is None and backend == "file":
+            return "Please provide a pasword to use for backend file"
+        cmd = self.based_cmd + f"--keyring-backend {backend} {key_name} --recover"
+        p = run(cmd, shell=True, encoding='ascii', stdout=PIPE, stderr=PIPE, input=f"{mnemonic}\n" if backend == "test" else (f"{mnemonic}\n" + f"{password}\n" * 2))
+        return f"{p.stdout} {p.stderr}"
